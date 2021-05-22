41f19599a52466c23b78b2f5b4ff3aac2ddc9a94
octawus <mihai_stanescu700@yahoo.es>
2021-05-22 13:32:39 +0200

    Did some work integrating firebase into the project. Also worked on the login interface.

--- app/src/main/java/com/example/appster/MainActivity.kt
+++ app/src/main/java/com/example/appster/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.appster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+// Firebase instance variables
+private lateinit var auth: FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
     }
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in.
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
+    }
+
+
+    // Probablemente lo pueda borrar
+    private fun getPhotoUrl(): String? {
+        val user = auth.currentUser
+        return user?.photoUrl?.toString()
+    }
+
+    private fun getUserName(): String? {
+        val user = auth.currentUser
+        return if (user != null) {
+            user.displayName
+        } else ANONYMOUS
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        signInClient.signOut()
+        startActivity(Intent(this, SignInActivity::class.java))
+        finish()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        const val MESSAGES_CHILD = "messages"
+        const val ANONYMOUS = "anonymous"
+        //Probablemente pueda borrar
+        private const val REQUEST_IMAGE = 2
+        private const val LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif"
+    }
+
 }
