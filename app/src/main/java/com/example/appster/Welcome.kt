213d0d8e86942ecd430a87ff807e8c836551f417
octawus <mihai_stanescu700@yahoo.es>
2021-05-28 19:47:21 +0200

    Added register and login with firebase UI

--- app/src/main/java/com/example/appster/Welcome.kt
+++ app/src/main/java/com/example/appster/Welcome.kt
@@ -1,9 +1,13 @@
 package com.example.appster
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class Welcome : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +25,27 @@ class Welcome : AppCompatActivity() {
                 .build(),
             RC_SIGN_IN
         )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 
     companion object{
